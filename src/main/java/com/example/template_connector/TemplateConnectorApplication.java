3622e0e7c4c575f1344d4aa99f2ea15631ff7a52
baguscripts <baguscripts@kali>
2025-02-23 10:55:05 +0300

    update

---
+++ src/main/java/com/example/template_connector/TemplateConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.template_connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/template_connector/TemplateConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.template_connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
